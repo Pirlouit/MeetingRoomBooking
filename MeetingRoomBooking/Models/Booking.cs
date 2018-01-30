1e783bf54a98fb2b100885d55fd7cb10beab8957
Pirlouit <thenkod@hotmail.com>
2018-01-30 09:39:19 +0100

    First Commit

---
+++ MeetingRoomBooking/Models/Booking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MeetingRoomBooking.Models
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        
+        public string BookingName { get; set; }
+
+        public string OwnerName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BookingDay { get; set; }
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH-mm}", ApplyFormatInEditMode = true)]
+        public TimeSpan BookingStart { get; set; }
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH-mm}", ApplyFormatInEditMode = true)]
+        public TimeSpan BookingEnd { get; set; }
+
+        public Room Room { get; set; }
+    }
+}
---
+++ MeetingRoomBooking/Models/MeetingRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeetingRoomBooking.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string RoomName { get; set; }
+        public ICollection<Booking> Bookings { get; set; }
+    }
+}
